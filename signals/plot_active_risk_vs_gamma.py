fd7883c9ff4dbee61315ea9592279ba2a4369359
Kyle Markham <kylecmarkham@gmail.com>
2025-11-19 13:13:02 -0700

    Added theoretical version for backtesting gamma vs ar

--- signals/plot_active_risk_vs_gamma.py
+++ signals/plot_active_risk_vs_gamma.py
@@ -11,8 +11,9 @@ from matplotlib import pyplot as plt
 import argparse
 from scipy.stats import linregress
 import sf_quant.optimizer as sfo
+import sf_quant.data as sfd
 
-def plot_active_risk_vs_gamma(signal='momentum', n=10, min=-1, max=2.5, start=dt.date.fromisoformat('2013-06-01'), end=dt.date.fromisoformat('2013-06-30'), historical=True):
+def plot_active_risk_vs_gamma(signal='momentum', n=10, min=0, max=6, start=dt.date.fromisoformat('2013-06-01'), end=dt.date.fromisoformat('2013-06-30'), historical=True):
     df = pl.read_parquet('../data/russell_3000_daily.parquet').sort('barrid', 'date') # .filter(pl.col('price').gt(5)) Temporarily removing price filter
     signals = add_signals.add_signals(df)
     returns = signals.lazy().with_columns(pl.col('return').shift(-1).alias('fwd_return')).filter(
@@ -21,23 +22,28 @@ def plot_active_risk_vs_gamma(signal='momentum', n=10, min=-1, max=2.5, start=dt
         (pl.col(f'{signal}_alpha').is_not_null())
         ).select(['date', 'barrid', f'{signal}_alpha', 'predicted_beta', 'fwd_return'
         ])
-    domain = np.logspace(min, max, n)
+    domain = np.logspace(min, max, n, base=np.e)
     active_risk = []
     for gamma in domain:
         if signal == 'bab':
-            gamma_weights = get_signal_weights.get_signal_weights(returns, signal, start, end, gamma=gamma, constraints=[sfo.UnitBeta()])
+            gamma_weights = get_signal_weights.get_signal_weights(returns, signal, start, end, gamma=gamma, constraints=[sfo.UnitBeta(), sfo.LongOnly()])
         else:
             gamma_weights = get_signal_weights.get_signal_weights(returns, signal, start, end, gamma=gamma)
 
         if historical:
             gamma_returns = get_signal_weights.get_returns_from_weights(gamma_weights.join(returns.select(['date', 'barrid', 'fwd_return']).collect(), on=['date', 'barrid']))
             active = gamma_returns.filter(pl.col('portfolio') == "active").with_columns(pl.col('return').truediv(100.))
-            active_risk.append(active.select(pl.col("return").std(ddof=1)).item() * np.sqrt(252.))
+            active_risk.append(active.select(pl.col("return").std()).item() * np.sqrt(252.))
         else:
             active_weights = get_signal_weights.get_active_weights_from_weights(gamma_weights)
-            # M: Finish this here by taking the sandwich product with the covariance matrix on each day, and averaging the active risks. 
-            # Then append to active_risk, and the plotting should just work as is.
-            # The formula is active risk = \sqrt{w_a^T \Sigma w_a}, I believe (and then annualize by multiplying by \sqrt{252}).
+            active_risks = []
+            for date in active_weights["date"].unique().sort().to_list():
+                barrids = active_weights.filter(pl.col('date').eq(date))['barrid'].unique().sort().to_list()
+                active_weight_on_date = active_weights.filter(pl.col('date').eq(date)).sort('barrid').select(pl.col('weights_act')).to_numpy().flatten()
+                cov = sfd.construct_covariance_matrix(date, barrids).drop('barrid').to_numpy()
+                active_risks.append(np.sqrt(active_weight_on_date[:, None] @ cov @ active_weight_on_date) * np.sqrt(252))
+            
+            active_risk.append(np.mean(active_risks))
 
     print_dict = {gamma: active_risk[i] for i, gamma in enumerate(domain)}
     print(print_dict)
@@ -54,7 +60,7 @@ def plot_active_risk_vs_gamma(signal='momentum', n=10, min=-1, max=2.5, start=dt
     plt.xlabel("Gamma")
     plt.ylabel("Active Risk")
     plt.legend()
-    plt.savefig(f'plots/log_regression_active_risk_vs_gamma_{signal}_{start}_{n}.png')
+    plt.savefig(f'plots/historical_log_regression_active_risk_vs_gamma_{signal}_{start}_{n}.png')
     plt.clf()
     plt.close("all")
 
@@ -70,4 +76,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f'Running plot_active_risk_vs_gamma with args={args}...')
 
-    plot_active_risk_vs_gamma(signal=args.signal, n=int(args.n), start=dt.date.fromisoformat(args.start), end=dt.date.fromisoformat(args.end))
+    plot_active_risk_vs_gamma(signal=args.signal, n=int(args.n), start=dt.date.fromisoformat(args.start), end=dt.date.fromisoformat(args.end), historical=True)
